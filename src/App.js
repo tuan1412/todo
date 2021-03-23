0bc24a0be1dbc0b8e76d2b3f3cb5f0e7695f4d79
tuan1412 <tuannguyenanh1412@gmail.com>
2021-03-23 23:32:26 +0700

    fix classname

--- src/App.js
+++ src/App.js
@@ -61,7 +61,7 @@ function App() {
               {tasks.map(task => (
                 <li key={task.id} className="list-group-item d-flex justify-content-between align-items-center">
                   <div>
-                    <input class="form-check-input me-2" type="checkbox" onChange={() => checkDoneTask(task.id)} />
+                    <input className="form-check-input me-2" type="checkbox" onChange={() => checkDoneTask(task.id)} />
                     <span className={task.isDone ? 'text-decoration-line-through' : ''}>{task.content}</span>
                   </div>
                   <button className="btn btn-danger btn-sm" onClick={() => deleteTask(task.id)}>Delete</button>
